4f9168819cb07519c0f284f484ba135e30369c18
charulagrl <charul.agrl@gmail.com>
2018-05-29 11:02:04 +0530

    Check user_id in request object and if account exists for that user_id

--- reddit/controller/upvote.py
+++ reddit/controller/upvote.py
@@ -5,7 +5,7 @@ from reddit.store import datastore
 from reddit.models.topic import Topic
 from reddit.models.upvote import Upvote
 from reddit.models.downvote import Downvote
-from reddit.app import datastore
+from reddit.app import app, datastore
 from reddit.utils.errors import internal_error
 from reddit.utils.errors import not_found
 from reddit.utils.errors import bad_request
@@ -20,12 +20,21 @@ def create_upvote(request_data):
 			app.logger.errors("Topic id is missing")
 			return bad_request("Topic id is missing")
 
+		if not request_data.get('user_id', None):
+			app.logger.error("User id is missing")
+			return bad_request("User id is missing")
+
 		topic_id = request_data['topic_id']
 		topic = datastore.topics.get(topic_id, None)
 
 		if not topic:
 			app.logger.errors("Topic with id %s does not exist"%topic_id)
 			return not_found("Topic with id %s does not exist"%topic_id)
+
+		user_id = request_data['user_id']
+		if not datastore.users.get(user_id, None):
+			app.logger.error("Account with user_id %s does not exist"%user_id)
+			return bad_request("Account with user_id %s does not exist"%user_id)
 
 		upvote_ob = datastore.upvotes[topic_id]
 		upvote_ob.upvotes += 1
